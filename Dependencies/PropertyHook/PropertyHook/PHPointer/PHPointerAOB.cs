daa9263527035b78fd24f9f7273339383777ad65
Scott Mooney <swj.mooney@gmail.com>
2022-05-10 09:55:56 -0400

    First commit

---
+++ Dependencies/PropertyHook/PropertyHook/PHPointer/PHPointerAOB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PropertyHook
+{
+    /// <summary>
+    /// A dynamic pointer starting from an array of bytes scanned for in the target process.
+    /// </summary>
+    public abstract class PHPointerAOB : PHPointer
+    {
+        /// <summary>
+        /// The AOB to scan for.
+        /// </summary>
+        public byte?[] AOB { get; set; }
+
+        /// <summary>
+        /// The result of the AOB scan.
+        /// </summary>
+        public IntPtr AOBResult;
+
+        /// <summary>
+        /// Creates a new AOB pointer.
+        /// </summary>
+        public PHPointerAOB(PHook parent, byte?[] aob, int[] offsets) : base(parent, offsets)
+        {
+            AOB = aob;
+        }
+
+        /// <summary>
+        /// Returns the result of the AOB scan.
+        /// </summary>
+        public override IntPtr ResolveSpecific()
+        {
+            return AOBResult;
+        }
+
+        /// <summary>
+        /// Scan for the AOB and store the adddress in `AOBResult`.
+        /// Returns `true` if the scan succeeded and `false` otherwise.
+        /// Called automatically by `PHook` for registered `PHPointerAOB`s.
+        /// </summary>
+        public abstract bool ScanAOB(AOBScanner scanner);
+
+        internal void DumpAOB()
+        {
+            AOBResult = IntPtr.Zero;
+        }
+    }
+}
---
+++ EldenRingSurvivalMode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoulsFormats;
+
+namespace EldenRingSurvivalMode
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
